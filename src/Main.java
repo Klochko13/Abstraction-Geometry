e0fc95aa1effce4645663377341191903942013a
Advokat <s.klochko1984@mail.ru>
2023-10-26 19:39:07 +0500

    Triangle

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,11 @@ public class Main {
         Square square = new Square(7);
         System.out.println("__________________________________\n");
         Rectangle rectangle = new Rectangle(3,6);
+        System.out.println("__________________________________\n");
+        Triangle triangle = new Triangle(12,6);
 
-    }
+
+        }
+
+
 }
